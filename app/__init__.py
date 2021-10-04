84c0482837cf1156357c8ce3d1c66836fb70cd24
Alex Leahu <alxjsn@sameorigin.org>
2021-10-04 13:57:56 -0500

    remove instance folder check, not used

--- app/__init__.py
+++ app/__init__.py
@@ -29,11 +29,19 @@ def create_app():
     app.config.from_object('app.config.DefaultConfig')
     CORS(app)
 
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
+    # there's probably a better way to make this distinction, but this works.
+
+-    if config == 'ProductionConfig':
+-        logging.basicConfig(
+-            filename='agora.log',
+-            level=logging.WARNING,
+-            format='%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s'
+-            )
+-    else:
+-        logging.basicConfig(
+-            level=logging.DEBUG,
+-            format='%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s'
+-            )
 
     # Add blueprints here.
     from . import agora
