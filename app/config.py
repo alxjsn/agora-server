a31bfc5dac300380a89d01df1c2c2fd5a4537551
Alex Leahu <alxjsn@sameorigin.org>
2021-10-10 17:14:37 -0500

    update search.xml based on config options, an other updates to config

--- app/config.py
+++ app/config.py
@@ -3,6 +3,7 @@ import os
 class DefaultConfig(object):
     AGORA_PATH = os.getenv('AGORA_PATH', '/home/agora/garden')
     AGORA_NAME = os.getenv('AGORA_NAME', 'thisagora')
+    AGORA_DESCRIPTION = os.getenv('AGORA_DESCRIPTION', 'An [[agora]] is an open distributed knowledge graph.')
     JOURNAL_ENTRIES = int(os.getenv('JOURNAL_ENTRIES', 10))
     RANK = os.getenv('RANK', 'agora').split(',')
     
